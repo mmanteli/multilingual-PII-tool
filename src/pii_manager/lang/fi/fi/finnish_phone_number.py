0da51d759b4fcd15a0d95f97dd3961dc476dd7db
Amanda Myntti <amanda.myntti@gmail.com>
2024-01-30 18:25:10 +0200

    Finnish added

---
+++ src/pii_manager/lang/fi/fi/finnish_phone_number.py
@@ -0,0 +1,25 @@
+"""
+Detection of phone numbers written with local phone number format (with our without country code), for DE
+"""
+
+
+from pii_manager import PiiEnum
+
+PATTERN_DE_PHONE = r'(((\+358 \d{2} )|(0\d{1,2} ))((\d{7,8})|(\d{2,4} \d{3,4})))|(0\d{3} (\d{3,4} \d{3,4}|\d{6,8}))'
+
+
+
+PII_TASKS = [
+    {
+        "pii": PiiEnum.PHONE_NUMBER,
+        "type": "regex",
+        "task": PATTERN_FI_PHONE,
+        "name": "Finnish phone number",
+        "doc": "detect phone numbers that use Finnish format. Uses language context",
+        "context": {
+            "value": ["puh.", "p.", "kotip.", "matkapuh.", "puhelin"],
+            "width": [22, 0],
+            "type": "word",
+        },
+    }
+]
