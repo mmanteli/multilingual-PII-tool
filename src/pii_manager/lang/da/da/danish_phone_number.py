73e6c05489a7f3827df5e5e080c863d2b5be5e59
mmanteli <amanda.myntti@gmail.com>
2024-02-06 13:18:04 +0200

    Danish added

---
+++ src/pii_manager/lang/da/da/danish_phone_number.py
@@ -0,0 +1,20 @@
+"""
+Detection of phone numbers written with local notation (with or without country code), for Danish format.
+"""
+
+
+from pii_manager import PiiEnum
+
+PATTERN_DA_PHONE = r'((\(\+45\)[ ]\d{2}[ ])|(\+45[ ]\d{2}[ ])|(\d{2}[ ]))\d{2}[ ]\d{2}[ ]\d{2}'
+
+
+
+PII_TASKS = [
+    {
+        "pii": PiiEnum.PHONE_NUMBER,
+        "type": "regex",
+        "task": PATTERN_DA_PHONE,
+        "name": "Danish phone number",
+        "doc": "detect phone numbers that use Danish format.",
+    }
+]
