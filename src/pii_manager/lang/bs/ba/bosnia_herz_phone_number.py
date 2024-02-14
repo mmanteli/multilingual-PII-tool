fa16f999c7d0715f202ced3af795e5eb4b6f7315
mmanteli <amanda.myntti@gmail.com>
2024-02-14 14:24:17 +0200

    Lithuanian and Bosnian added

---
+++ src/pii_manager/lang/bs/ba/bosnia_herz_phone_number.py
@@ -0,0 +1,20 @@
+"""
+Detection of phone numbers written with local notation (with or without country code), for Bosnian-Herzegovinian format.
+"""
+
+
+from pii_manager import PiiEnum
+
+PATTERN_BA_PHONE = r'((\+[ ]?387[ ]\d{2}[ ])|(0[ ]?\d{2}[ ]))\d{3}[ -]\d{3}'
+
+
+
+PII_TASKS = [
+    {
+        "pii": PiiEnum.PHONE_NUMBER,
+        "type": "regex",
+        "task": PATTERN_BA_PHONE,
+        "name": "Bosnian-Herzegovinian phone number",
+        "doc": "detect phone numbers that use Bosnian-Herzegovinian format.",
+    }
+]
---
+++ src/pii_manager/lang/lt/lt/lithuanian_phone_number.py
@@ -0,0 +1,20 @@
+"""
+Detection of phone numbers written with local notation (with or without country code), for Lithuanian format.
+"""
+
+
+from pii_manager import PiiEnum
+
+PATTERN_LT_PHONE = r'((\+370[ ]\d{1,2}[ ])|(\+370[ ])|(\(\d{2,4}\)[ ]))((\d{2,3}[ ]\d{4})|(\d{3}[ ]\d{2}[ ]\d{2}))|(\(370[ ]\d\)[ ]\d{3}[ ]\d{4})'
+
+
+
+PII_TASKS = [
+    {
+        "pii": PiiEnum.PHONE_NUMBER,
+        "type": "regex",
+        "task": PATTERN_LT_PHONE,
+        "name": "Lithuanian phone number",
+        "doc": "detect phone numbers that use Lithuanian format.",
+    }
+]
