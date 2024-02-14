7db93e523d3337185626238a0b521d3b9ea22168
mmanteli <amanda.myntti@gmail.com>
2024-02-14 14:16:38 +0200

    Bulgarian added

---
+++ src/pii_manager/lang/bg/bg/bulgarian_phone_number.py
@@ -0,0 +1,20 @@
+"""
+Detection of phone numbers written with local notation (with or without country code), for Bulgarian format.
+"""
+
+
+from pii_manager import PiiEnum
+
+PATTERN_BG_PHONE = r'(((\+359[ ]?\d{1,3}[ ]?)|(0\d[ \/][ ]?))\d{1,3}[ ]?\d{3,4})|(\+359[ ]\(0\)\d\/\d[ ]\d{4,6})'
+
+
+
+PII_TASKS = [
+    {
+        "pii": PiiEnum.PHONE_NUMBER,
+        "type": "regex",
+        "task": PATTERN_BG_PHONE,
+        "name": "Bulgarian phone number",
+        "doc": "detect phone numbers that use Bulgarian format.",
+    }
+]
