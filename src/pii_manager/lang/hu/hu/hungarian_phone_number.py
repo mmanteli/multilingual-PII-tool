db59603720df8e14e0c7834606b4a76b4aac1ef3
mmanteli <amanda.myntti@gmail.com>
2024-03-08 10:46:30 +0200

    Modified bg, hu, and nl based on false negatives

--- src/pii_manager/lang/hu/hu/hungarian_phone_number.py
+++ src/pii_manager/lang/hu/hu/hungarian_phone_number.py
@@ -1,11 +1,13 @@
 """
-Detection of phone numbers written with local notation (with or without country code), for Netherlands format.
+Detection of phone numbers written with local notation (with or without country code), for Hungarian format.
 """
 
 
 from pii_manager import PiiEnum
 
-PATTERN_HU_PHONE = r'((\+36[ ])|(00[ ]36[ ])|(\d{2}[ ]))(\d{1,2}[ ]\d{3}[ ]\d{3,4})'
+PATTERN_HU_PHONE = r'((\(\+36[ -]\d{1,2}\))|(\+36[ -]\(\d{1,2}\))|(\+36([ -/]|[ ]-[ ])\d{1,2})|(0\d[ \/]\d{2}))[- \/]?((\d{1}[ -]\d{3}[ -]\d{3,4})|(\d{3}[ -]\d{2}[ -]\d{2})|(\d{3}[- ]\d{3,4})|(\d{3,4}[- ]\d{3}))|((\+36[ ])|(00[ ]?36[ ])|(\d{2}[ -\/]))(\d{1,2}[ -]\d{3}[ -]\d{3,4})'
+# updated based on false negatives
+#PATTERN_HU_PHONE = r'((\+36[ ])|(00[ ]36[ ])|(\d{2}[ ]))(\d{1,2}[ ]\d{3}[ ]\d{3,4})'
 
 
 
